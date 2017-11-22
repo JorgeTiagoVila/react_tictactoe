47eafa534a66539878925f47e3b0bb865d9693ac
filipecorreia <filipe.correia@elecctro.com>
2017-11-22 13:05:43 +0000

    Added tie support.
    (cherry picked from commit ec7e202)

--- src/containers/BoardContainer.js
+++ src/containers/BoardContainer.js
@@ -54,6 +54,19 @@ class BoardContainer extends Component {
                 return squares[a];
             }
         }
+
+        let hasEmptySquare = false;
+        for (let i = 0; i < 8; i++) {
+            if (squares[i] === null) {
+                hasEmptySquare = true;
+                break;
+            }
+        }
+
+        if (!hasEmptySquare) {
+            return 'None'
+        }
+
         return null;
     }
 
