9d00da42f26cd74f6b17db462dbf9c1a8035a966
filipecorreia <filipe.correia@elecctro.com>
2017-11-23 01:18:44 +0000

    Whoopsy...

--- src/containers/GameListContainer.js
+++ src/containers/GameListContainer.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { bindActionsCreators } from 'redux';
+import { bindActionCreators } from 'redux';
 import { connect } from 'react-redux';
 import uuid from 'uuid-v4';
 
