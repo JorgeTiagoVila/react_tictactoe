b8f18de025e4fe322d314c7d1d5598ee7bb657b4
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-11-30 13:25:08 +0000

    Migrated GameListHeader to Semantic UI.

--- src/components/GameListHeader.js
+++ src/components/GameListHeader.js
@@ -1,36 +1,54 @@
 import React from 'react';
+import { Dropdown, Header, Label } from 'semantic-ui-react'
 
 const GameListHeader = (props) =>
     <div className="gameListHeader">
-        <b>Winner:</b>
+        <Header size='tiny'>Filter Winner:</Header>
         <div>
-            <input
-                checked={props.filters.includes('X')}
-                onChange={() => props.onToggleFilter('X')}
-                type="checkbox"
-            />X
-            <input
-                checked={props.filters.includes('O')}
-                onChange={() => props.onToggleFilter('O')}
-                type="checkbox"
-            />O
-            <input
-                checked={props.filters.includes('None')}
-                onChange={() => props.onToggleFilter('None')}
-                type="checkbox"
-            />None
+            <Label
+                as="a"
+                color="blue"
+                active={props.filters.includes('X')}
+                onClick={() => props.onToggleFilter('X')}
+            >
+                X Won
+            </Label>
+            <Label
+                as="a"
+                color="red"
+                active={props.filters.includes('O')}
+                onClick={() => props.onToggleFilter('O')}
+            >
+                O Won
+            </Label>
+            <Label
+                as="a"
+                color="yellow"
+                active={props.filters.includes('None')}
+                onClick={() => props.onToggleFilter('None')}
+            >
+                Tie
+            </Label>
         </div>
-        <br />
-        <b>Sort By:&nbsp;</b>
-        <select
-            onChange={(event) => props.onChangeSortField(event.target.value)}
-            value={props.sortField}
-        >
-            <option value="-gameStart">&darr; Start date</option>
-            <option value="gameStart">&uarr; Start date</option>
-            <option value="-duration">&darr; Duration</option>
-            <option value="duration">&uarr; Duration</option>
-        </select>
+        <Header size='tiny'>Sort By</Header>
+        <Dropdown fluid selection options={[{
+            text: 'Start date',
+            value: '-gameStart',
+            icon: 'triangle down'
+        }, {
+            text: 'Start date',
+            value: 'gameStart',
+            icon: 'triangle up'
+        }, {
+            text: 'Duration',
+            value: '-duration',
+            icon: 'triangle down'
+        }, {
+            text: 'Duration',
+            value: 'duration',
+            icon: 'triangle up'
+        }
+        ]} />
     </div>;
 
 export default GameListHeader;
--- src/components/Square.js
+++ src/components/Square.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const Square = (props) =>
-    <button className="square" onClick={props.onClick}>
+    <button
+        className="square"
+        onClick={props.onClick}
+        style={{ color: props.value === 'X' ? '#2185d0' : '#db2828' }}
+    >
         {props.value}
     </button>;
 
