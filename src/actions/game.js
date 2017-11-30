8ad611f95e99cf7e4d18dc0d4ce2f22834594d94
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-11-30 11:16:12 +0000

    Added actions to set the game list sort field and to toggle filters.

--- src/actions/game.js
+++ src/actions/game.js
@@ -1,11 +1,23 @@
 export const actionTypes = {
-    SET_SELECTED_GAME: 'GAME/SELECTED_GAME',
+    SET_SELECTED_GAME: 'GAME/SET_SELECTED_GAME',
+    SET_SORT_FIELD: 'GAME/SET_SORT_FIELD',
+    TOGGLE_FILTER: 'GAME/TOGGLE_FILTER',
     ADD_GAME: 'GAME/ADD_GAME',
 };
 
 export const setSelectedGame = (gameId) => ({
     type: actionTypes.SET_SELECTED_GAME,
     payload: gameId,
+});
+
+export const setSortField = (field) => ({
+    type: actionTypes.SET_SORT_FIELD,
+    payload: field,
+});
+
+export const toggleFilter = (filter) => ({
+    type: actionTypes.TOGGLE_FILTER,
+    payload: filter,
 });
 
 export const addGame = (gameId, game) => ({
