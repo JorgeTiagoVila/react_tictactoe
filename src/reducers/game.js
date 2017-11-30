0b26d77fded66c0eb83324fe9e12b0e573cd27ff
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-11-30 11:17:22 +0000

    Implemented logic to handle to toggle filter and the set sort field actions to the game reducer.

--- src/reducers/game.js
+++ src/reducers/game.js
@@ -3,6 +3,8 @@ import { actionTypes } from '../actions/game';
 const initialState = {
     games: [],
     selectedGame: null,
+    filters: ['X', 'O', 'None'],
+    sortField: '-gameStart'
 };
 
 const game = (state = initialState, action) => {
@@ -14,6 +16,20 @@ const game = (state = initialState, action) => {
             const games = state.games.slice();
             games.unshift([action.payload.gameId, action.payload.game]);
             return Object.assign({}, state, { games });
+
+        case actionTypes.TOGGLE_FILTER:
+            const filters = state.filters.slice();
+            const filterIndex = filters.indexOf(action.payload);
+            if (filterIndex === -1) {
+                filters.push(action.payload);
+            } else {
+                filters.splice(filterIndex, 1);
+            }
+
+            return Object.assign({}, state, { filters });
+
+        case actionTypes.SET_SORT_FIELD:
+            return Object.assign({}, state, { sortField: action.payload });
 
         default:
             return state;
