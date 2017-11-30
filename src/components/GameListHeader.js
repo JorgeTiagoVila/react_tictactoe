1041695ca49f2b59b9e2ec2368afb7daa374e380
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-11-30 11:18:04 +0000

    Created the GameListHeader component.

---
+++ src/components/GameListHeader.js
@@ -0,0 +1,36 @@
+import React from 'react';
+
+const GameListHeader = (props) =>
+    <div className="gameListHeader">
+        <b>Winner:</b>
+        <div>
+            <input
+                checked={props.filters.includes('X')}
+                onClick={() => props.onToggleFilter('X')}
+                type="checkbox"
+            />X
+            <input
+                checked={props.filters.includes('O')}
+                onClick={() => props.onToggleFilter('O')}
+                type="checkbox"
+            />O
+            <input
+                checked={props.filters.includes('None')}
+                onClick={() => props.onToggleFilter('None')}
+                type="checkbox"
+            />None
+        </div>
+        <br />
+        <b>Sort By:&nbsp;</b>
+        <select
+            onChange={(event) => props.onChangeSortField(event.target.value)}
+            value={props.sortField}
+        >
+            <option value="-gameStart">&darr; Start date</option>
+            <option value="gameStart">&uarr; Start date</option>
+            <option value="-duration">&darr; Duration</option>
+            <option value="duration">&uarr; Duration</option>
+        </select>
+    </div>;
+
+export default GameListHeader;
