4e80f90c423d220ccb1fddd17a8e9618f1a752d0
Jorge Tiago Vila <jorge.vila@elecctro.com>
2017-11-30 13:02:30 +0000

    Switched the ScoreTableContainer to Semantic UI.

--- src/containers/ScoreTableContainer.js
+++ src/containers/ScoreTableContainer.js
@@ -1,37 +1,30 @@
-import React, { Component } from 'react';
-import { connect } from 'react-redux';
+import React from 'react';
+import { connect } from 'react-redux';
+import { Icon, Statistic } from 'semantic-ui-react'
 
-import { getScores } from '../selectors/game';
+import { getScores } from '../selectors/game';
 
-import ScoreCard from '../components/ScoreCard';
-
-class ScoreTableContainer extends Component {
-    render() {
-        const { scores } = this.props;
-
-        return (
-            <div className="scoreTableContainer">
-                <ScoreCard
-                    title="Games Played"
-                    value={scores.gamesPlayed}
-                />
-                <ScoreCard
-                    title="Games X Won"
-                    value={scores.gamesXWon}
-                />
-                <ScoreCard
-                    title="Games O Won"
-                    value={scores.gamesOWon}
-                />
-
-                <ScoreCard
-                    title="Games Tied"
-                    value={scores.gamesTied}
-                />
-            </div>
-        );
-    }
-}
+const ScoreTableContainer = (props) =>
+    <Statistic.Group className="scoreTableContainer" widths='four'>
+        <Statistic>
+            <Statistic.Value>
+                <Icon name='game' />&nbsp;{props.scores.gamesPlayed}
+            </Statistic.Value>
+            <Statistic.Label>Games Played</Statistic.Label>
+        </Statistic>
+        <Statistic>
+            <Statistic.Value>{props.scores.gamesXWon}</Statistic.Value>
+            <Statistic.Label>X Wins</Statistic.Label>
+        </Statistic>
+        <Statistic>
+            <Statistic.Value>{props.scores.gamesOWon}</Statistic.Value>
+            <Statistic.Label>O Wins</Statistic.Label>
+        </Statistic>
+        <Statistic>
+            <Statistic.Value>{props.scores.gamesTied}</Statistic.Value>
+            <Statistic.Label>Ties</Statistic.Label>
+        </Statistic>
+    </Statistic.Group>;
 
 const mapStateToProps = (state) => ({
     scores: getScores(state),
